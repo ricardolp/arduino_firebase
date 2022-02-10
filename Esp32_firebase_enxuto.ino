9dab1bfed4232cbe79ae9c84cb2ed8a2ceabca1e
Ricardo <ricardoluiz_p@hotmail.com>
2022-02-10 11:05:09 -0300

    Add files via upload

---
+++ Esp32_firebase_enxuto.ino
@@ -0,0 +1,96 @@
+#include <WiFi.h>         
+#include <IOXhop_FirebaseESP32.h>                           
+#include <ArduinoJson.h>                   
+
+#define WIFI_SSID "VIVOFIBRA-2BE3"                   
+#define WIFI_PASSWORD "f2HgTNCbZB"         
+#define FIREBASE_HOST "https://arduino-fc608-default-rtdb.firebaseio.com/"    
+#define FIREBASE_AUTH "CpNZObP6W9Jul61u5XPM9ITzAlVDzvn9oIH0CeQS"   
+
+const int pin = 4;
+
+void setup() {
+
+  pinMode(pin, OUTPUT);
+  digitalWrite(pin, HIGH);
+  
+  Serial.begin(115200);
+  Serial.println();
+
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  
+  Serial.print("Conectando ao wifi");
+  
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(300);
+  }
+  
+  Serial.println();
+
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);
+}
+
+
+void loop() {
+  
+  Serial.println();
+
+  if (Firebase.getString("/quarto/luminosidade") != "acesa") {
+    digitalWrite(pin, LOW); 
+    Serial.print("A luz esta apagada");
+  } else {
+    digitalWrite(pin, HIGH);
+    Serial.print("A luz esta acesa");
+  }
+
+  /*
+  Serial.println();
+  Serial.print("Dono do quarto: ");
+  Serial.print(Firebase.getString("/quarto/dono"));
+  Serial.println();
+  Serial.print("Luminosidade do quarto: ");
+  Serial.print(Firebase.getInt("/quarto/luminosidade"));
+  Serial.println();
+  Serial.print("Temperatura do quarto: ");
+  Serial.print(Firebase.getFloat("/quarto/temperatura"));
+  Serial.println();
+  Serial.print("O quarto está ocupado: ");
+  Serial.print(Firebase.getBool("/quarto/ocupado"));
+  Serial.println();
+  Serial.print("Luminosidade da sala: ");
+  Serial.print(Firebase.getInt("/sala/luminosidade"));
+  Serial.println();
+  delay(3000);
+*/
+
+/*
+  Firebase.setString("/quarto/dono", "Rebeca");
+  Firebase.setInt("/quarto/luminosidade", 300);
+  Firebase.setBool("/quarto/ocupado", false);
+  Firebase.setFloat("/quarto/temperatura", 24.7);
+  Firebase.setInt("/sala/luminosidade", 200);
+  delay(3000);
+  Firebase.setString("/quarto/dono", "Matteo");
+  Firebase.setInt("/quarto/luminosidade", 500);
+  Firebase.setBool("/quarto/ocupado", true);
+  Firebase.setFloat("/quarto/temperatura", 35.3);
+  Firebase.setInt("/sala/luminosidade", 500);
+  delay(3000);
+*/
+
+/*
+  Firebase.pushString("/quarto/registro", "Matteo");
+  delay(3000);
+  Firebase.pushString("/quarto/registro", "Rebeca");
+  delay(3000);
+  Firebase.pushString("/quarto/registro", "Vanderson");
+  delay(3000);
+  Firebase.pushString("/quarto/registro", "Raquel");
+  delay(3000);
+*/
+
+  delay(100);
+
+}
